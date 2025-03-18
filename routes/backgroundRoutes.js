6f92a489a80c9a1271df142c182de1bd4931ca40
laTorresGH <laureano26112001@hotmail.com>
2025-03-18 04:17:19 -0300

    cambios background

--- routes/backgroundRoutes.js
+++ routes/backgroundRoutes.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/uploadBackground");
 const backgroundController = require("../controllers/backgroundController");
+const path = require("path"); // Importa el módulo 'path'
+const fs = require("fs"); // Importa el módulo 'fs'
+
+// Ruta estática para servir imágenes desde la carpeta 'public/background'
+router.use('/background', express.static(path.join(__dirname, '../public/background')));
 
 router.post("/background", upload.single("background"), backgroundController.uploadBackground);
 router.get("/background", backgroundController.getBackground);
 router.get("/backgrounds", backgroundController.getAllBackgrounds);
 
+// No necesitas esta ruta si ya usas express.static, pero si decides hacerlo manualmente:
+router.get('/background/:filename', (req, res) => {
+  const filePath = path.join(__dirname, '../public/background', req.params.filename);
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return res.status(404).send('Imagen no encontrada');
+    }
+    res.sendFile(filePath);
+  });
+});
 
 module.exports = router;
